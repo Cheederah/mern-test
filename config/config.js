922fd3f4fa9203547688d9295f29ecaa5522a197
Cheederah <misscheederah@gmail.com>
2021-01-08 08:42:07 +0100

    adding project files

---
+++ config/config.js
@@ -0,0 +1,31 @@
+// const config = {
+//     env: process.env.NODE_ENV || 'development',
+//     port: process.env.PORT || 3000,
+//     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+//     // mongoUri: process.env.MONGODB_URI ||
+//     // 'mongodb+srv://chidera:chidera@mernsetup.haboq.mongodb.net/Mernsetup?retryWrites=true&w=majority',
+//     // 'mongodb://chidera:chidera@mernsetup.haboq.mongodb.net:27017/admin'
+//     //     process.env.MONGO_HOST || 'mongodb://' + (process.env.IP || 'localhost') + ':' +
+//     //     (process.env.MONGO_PORT || '27017') + '/Mernsetup'
+
+//     mongoUri: process.env.MONGODB_URI ||
+//     process.env.MONGO_HOST ||
+//     'mongodb://' + (process.env.IP || 'localhost') + ':' +
+//     (process.env.MONGO_PORT || '27017') +
+//     '/test'
+// }
+
+// export default config;
+
+const config = {
+    env: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 3000,
+    jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+    mongoUri: process.env.MONGODB_URI ||
+      process.env.MONGO_HOST ||
+      'mongodb://' + (process.env.IP || 'localhost') + ':' +
+      (process.env.MONGO_PORT || '27017') +
+      '/mernproject'
+  }
+  
+  export default config;
